e34a9329203b91a33349c49b871536844676b0f1
lohotshreya <lohotshreya@gmail.com>
2026-02-09 18:21:30 +0530

    Add number search functionality in a list

---
+++ mini_exercises/09_search_in_list.py
@@ -0,0 +1,7 @@
+numbers = [10, 25, 7, 18, 30]
+key = int(input("Enter number to search: "))
+
+if key in numbers:
+    print("Number found in the list")
+else:
+    print("Number not found in the list")
